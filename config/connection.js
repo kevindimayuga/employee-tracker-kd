ce2ff125c5f73fb85445e59385dfc07ff4900904
kevindimayuga <k.dimayuga22@gmail.com>
2023-08-29 10:55:05 -0500

    updated sql files and connection.js file

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+// Import and require mysql2
+const mysql = require('mysql2');
+
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: process.env.MYSQL_PASSWORD,
+  database: 'employees_db'
+});
+
+module.exports = connection;
