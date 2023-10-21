61e1d8f2438bc20bf359c78c4c13b096875be3d6
kevindimayuga <k.dimayuga22@gmail.com>
2023-10-21 04:35:05 -0500

    began server.js code

---
+++ server.js
@@ -0,0 +1,7 @@
+// import necessary packages
+const express = require('express');
+const mysql = require('mysql2');
+
+// set up port and express app
+const PORT = process.env.PORT || 3001;
+const app = express();
