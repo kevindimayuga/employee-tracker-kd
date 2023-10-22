04ef93eff0645be3e6fb44d15cf8378109c0886c
kevindimayuga <k.dimayuga22@gmail.com>
2023-10-22 17:27:28 -0500

    added data to seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO department (name)
+VALUES ("Marketing"),
+       ("Finance"),
+       ("Human Resources"),
+       ("IT"),
+       ("Accounting");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Marketing Manager", 100000, 1),
+       ("Sales Associate", 50000, 1),
+       ("Finance Manager", 150000, 2),
+       ("Financial Analyst", 75000, 2),
+       ("Human Resources Manager", 125000, 3),
+       ("Human Resources Associate", 60000, 3),
+       ("IT Manager", 175000, 4),
+       ("Software Developer", 85000, 4),
+       ("Accounting Manager", 200000, 5),
+       ("Audit Accountant", 100000, 5);
