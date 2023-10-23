9a9e5cf51b15d964345d6d8f6f818f5870cd438c
kevindimayuga <k.dimayuga22@gmail.com>
2023-10-22 19:07:14 -0500

    added create table for employees and the properties

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employees_db;
+
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+-- This will create the department table and it will have an id column that is an integer
+-- and is the primary key and it will auto increment. It will also have a department_name
+-- column that is a varchar and is not null.
+CREATE TABLE
+    department (
+        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        department_name VARCHAR(30) NOT NULL
+    );
+
+-- Similarly, this will create a role table
+CREATE TABLE
+    role (
+        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        title VARCHAR(30) NOT NULL,
+        salary DECIMAL(10, 2) NOT NULL,
+        department_id INTEGER
+    );
+
+-- Similarly, this will create an employee table
+CREATE TABLE
+    employee (
+        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        first_name VARCHAR(30) NOT NULL,
+        last_name VARCHAR(30) NOT NULL,
+        role_id INTEGER,
+        manager_id INTEGER
+    );
