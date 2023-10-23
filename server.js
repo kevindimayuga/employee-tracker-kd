859adb74bf123b599009e532b14b22a305335557
kevindimayuga <k.dimayuga22@gmail.com>
2023-10-22 22:20:52 -0500

    created connection to employee database using .env file

--- server.js
+++ server.js
@@ -5,3 +5,15 @@ const mysql = require('mysql2');
 // set up port and express app
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// set up and connect to database
+const db = mysql.createConnection(
+    {
+        // use a .env file to pull in sensitive data
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    },
+    console.log(`Connected to the ${process.env.DB_NAME} database.`)
+);
